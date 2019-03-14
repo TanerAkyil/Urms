e78ca1ad50326ef037d0c76922d4258c8343bc60
Bünyamin Macit <macitbunyamin@gmail.com>
2019-03-14 15:11:01 +0300

    log out çalısıyor

---
+++ Urms.Admin/Controllers/DesignationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Urms.Admin.Controllers
+{
+    public class DesignationController : Controller
+    {
+        // GET: Designation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
