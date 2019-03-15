0a57ba1fb0c7e71cb7f6b2d7d1ffeb3eecd420f9
Bünyamin Macit <macitbunyamin@gmail.com>
2019-03-15 14:25:04 +0300

    log out ve wievler yapıldı

--- Urms.Model/Department.cs
+++ Urms.Model/Department.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,10 @@ namespace Urms.Model
 
 
 
-
-         public string DeptCode { get; set; }
-         public string DeptName { get; set; }
+        [Display(Name = "Departman Kodu")]
+        public string DeptCode { get; set; }
+        [Display(Name = "Departman Kodu")]
+        public string DeptName { get; set; }
 
         public virtual ICollection<Course> Courses { get; set; }
         public virtual ICollection<Teacher> Teachers { get; set; }
